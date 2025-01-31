f5128af2fe208d8ea0848b75c86036d1df69a5c8
konjit <kkll6349@gmail.com>
2025-01-31 23:02:58 +0100

    Async/await prep exercise solution

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -8,13 +8,32 @@ const DANCING_CAT_URL =
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    img.style.left = `${startPos}px`;
+
+    const step = () => {
+    let currentCatPos = parseInt(img.style.left||'0px');
+    if(currentCatPos < stopPos){
+      currentCatPos += STEP_SIZE_PX;
+      img.style.left = `${currentCatPos}px`
+    }else{
+      clearInterval(catWalking);
+      resolve();
+    }
+  }
+
+  const catWalking = setInterval(step, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+   const savedImg = img.src;
+   img.src = DANCING_CAT_URL;
+   setTimeout(()=>{
+    img.src = savedImg;
+    resolve();
+   }, DANCE_TIME_MS)
+
   });
 }
 
@@ -24,7 +43,12 @@ async function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  while(true){
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos, stopPos);
+    img.style.left = '0px';
+  }
 }
 
 window.addEventListener('load', catWalk);
